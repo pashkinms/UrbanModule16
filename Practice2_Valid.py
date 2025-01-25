b6480cdb43cedc7cb2c9e6134574151fadd22b40
AlcheMystic <pashkinms86@gmail.com>
2025-01-26 06:55:47 +0700

    Task2 Modified

--- Practice2_Valid.py
+++ Practice2_Valid.py
@@ -16,8 +16,8 @@ async def user_page(user_id: int = Path(ge=1, le=100, title='Unique ID for each 
     return {'message': f'Вы вошли как пользователь №{user_id}'}
 
 @app.get('/user/{username}/{age}')
-async def user_info(username:Annotated[str, Path(min_length=5, max_length=20, description='Не более 20 символов')], 
-                    age: Annotated[int, Path(ge=18, le=120, title='User age 18+')]):
+async def user_info(username:Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')], 
+                    age: Annotated[int, Path(ge=18, le=120, title='User age 18+', description='enter age', example='24')]):
     return {'Информация о пользователе:': f'Имя: {username} Возраст: {age}'}
     
     
